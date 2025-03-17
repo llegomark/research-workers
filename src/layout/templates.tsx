840e660be497abe093d96f58f92b59c554d1b1ba
Mark Anthony Llego <markllego@gmail.com>
2025-03-17 12:42:09 +0800

    Update font stylesheet to include variable font support for improved typography

--- src/layout/templates.tsx
+++ src/layout/templates.tsx
@@ -110,7 +110,7 @@ export const Layout: FC = (props) => {
 				<link href="/styles.css" rel="stylesheet" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono&display=swap" rel="stylesheet" />
+				<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
 
 				{/* Inline SVG favicon - works without external assets */}
 				<link rel="icon" href={`data:image/svg+xml;base64,${btoa(InlineFavicon)}`} type="image/svg+xml" />
